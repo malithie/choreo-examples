093d277f74ea4e78d36cb699bab03b0f71752373
Malithi Madara Edirisinghe <malithim@wso2.com>
2023-12-03 22:13:02 -0600

    Update config.js

--- b2c-apps/pet-care-app/pet-management-webapp/config.js
+++ b2c-apps/pet-care-app/pet-management-webapp/config.js
@@ -1,8 +1,8 @@
 window.config = {
-    baseUrl: "https://is.wso2isdemo.com",
-    clientID: "BfeQanf9JGMmDXawOIybWQgqyQEa",
+    baseUrl: "",
+    clientID: "",
     signInRedirectURL: "http://127.0.0.1:5173/",
     signOutRedirectURL: "http://127.0.0.1:5173/",
-    resourceServerURL: "https://2fb778b1-7130-4208-a3f8-d87659ebb85e-dev.e1-us-east-azure.choreoapis.dev/aydo/pet-management-service/pet-management-service-803/1.0.0",
+    resourceServerURL: "",
     scope: ["openid", "email", "profile"]
     };
